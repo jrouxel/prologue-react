f49121bea211b56957dd880d4145c8a93021a7fb
Emile Jutras <emilejutras@live.fr>
2023-07-06 17:03:48 -0400

    huh?

--- src/components/SpeechArea.js
+++ src/components/SpeechArea.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import "./SpeechAreaStyle.css";
 
 const SpeechArea = () => {
   return (
-    <div>User Speech Area</div>
+<div class="speech-to-text">
+  <div class="speech-to-text2">
+    <span>
+      <span class="speech-to-text-2-span">What is Art Souterrain?</span>
+      <span class="speech-to-text-2-span2"> </span>
+      <span class="speech-to-text-2-span3">Abstract design ?</span></span>
+  </div>
+</div>
   )
 };
 
