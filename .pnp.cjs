d37b32f111733968f8a47f8713bfe98ae8880e1d
FernandoSLuz <51529517+FernandoSLuz@users.noreply.github.com>
2023-07-07 15:45:50 -0400

    updates

--- .pnp.cjs
+++ .pnp.cjs
@@ -32,10 +32,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/jest-dom", "npm:5.16.5"],\
             ["@testing-library/react", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:13.5.0"],\
-            ["lottie-react", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:2.4.0"],\
             ["microsoft-cognitiveservices-speech-sdk", "npm:1.30.1"],\
+            ["prop-types", "npm:15.8.1"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:18.2.0"],\
+            ["react-lottie", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:1.2.3"],\
             ["react-redux", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:8.1.1"],\
             ["react-scripts", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:5.0.1"],\
             ["redux", "npm:4.2.1"],\
@@ -6429,6 +6430,17 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["babel-runtime", [\
+        ["npm:6.26.0", {\
+          "packageLocation": "./.yarn/cache/babel-runtime-npm-6.26.0-d38e7946b4-8aeade9466.zip/node_modules/babel-runtime/",\
+          "packageDependencies": [\
+            ["babel-runtime", "npm:6.26.0"],\
+            ["core-js", "npm:2.6.12"],\
+            ["regenerator-runtime", "npm:0.11.1"]\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["balanced-match", [\
         ["npm:1.0.2", {\
           "packageLocation": "./.yarn/cache/balanced-match-npm-1.0.2-a53c126459-9706c088a2.zip/node_modules/balanced-match/",\
@@ -7169,6 +7181,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["core-js", [\
+        ["npm:2.6.12", {\
+          "packageLocation": "./.yarn/unplugged/core-js-npm-2.6.12-0b93d77d31/node_modules/core-js/",\
+          "packageDependencies": [\
+            ["core-js", "npm:2.6.12"]\
+          ],\
+          "linkType": "HARD"\
+        }],\
         ["npm:3.31.1", {\
           "packageLocation": "./.yarn/unplugged/core-js-npm-3.31.1-7df9ac7f4f/node_modules/core-js/",\
           "packageDependencies": [\
@@ -11934,33 +11953,6 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
-      ["lottie-react", [\
-        ["npm:2.4.0", {\
-          "packageLocation": "./.yarn/cache/lottie-react-npm-2.4.0-d4d95c55aa-e9ea4a89be.zip/node_modules/lottie-react/",\
-          "packageDependencies": [\
-            ["lottie-react", "npm:2.4.0"]\
-          ],\
-          "linkType": "SOFT"\
-        }],\
-        ["virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:2.4.0", {\
-          "packageLocation": "./.yarn/__virtual__/lottie-react-virtual-16b573e0d6/0/cache/lottie-react-npm-2.4.0-d4d95c55aa-e9ea4a89be.zip/node_modules/lottie-react/",\
-          "packageDependencies": [\
-            ["lottie-react", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:2.4.0"],\
-            ["@types/react", null],\
-            ["@types/react-dom", null],\
-            ["lottie-web", "npm:5.12.2"],\
-            ["react", "npm:18.2.0"],\
-            ["react-dom", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:18.2.0"]\
-          ],\
-          "packagePeers": [\
-            "@types/react-dom",\
-            "@types/react",\
-            "react-dom",\
-            "react"\
-          ],\
-          "linkType": "HARD"\
-        }]\
-      ]],\
       ["lottie-web", [\
         ["npm:5.12.2", {\
           "packageLocation": "./.yarn/cache/lottie-web-npm-5.12.2-942dc9e22a-af5bc3bc40.zip/node_modules/lottie-web/",\
@@ -14865,10 +14857,11 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@testing-library/jest-dom", "npm:5.16.5"],\
             ["@testing-library/react", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:13.4.0"],\
             ["@testing-library/user-event", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:13.5.0"],\
-            ["lottie-react", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:2.4.0"],\
             ["microsoft-cognitiveservices-speech-sdk", "npm:1.30.1"],\
+            ["prop-types", "npm:15.8.1"],\
             ["react", "npm:18.2.0"],\
             ["react-dom", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:18.2.0"],\
+            ["react-lottie", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:1.2.3"],\
             ["react-redux", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:8.1.1"],\
             ["react-scripts", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:5.0.1"],\
             ["redux", "npm:4.2.1"],\
@@ -15160,6 +15153,30 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "linkType": "HARD"\
         }]\
       ]],\
+      ["react-lottie", [\
+        ["npm:1.2.3", {\
+          "packageLocation": "./.yarn/cache/react-lottie-npm-1.2.3-3d4f0a3852-05326e40b4.zip/node_modules/react-lottie/",\
+          "packageDependencies": [\
+            ["react-lottie", "npm:1.2.3"]\
+          ],\
+          "linkType": "SOFT"\
+        }],\
+        ["virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:1.2.3", {\
+          "packageLocation": "./.yarn/__virtual__/react-lottie-virtual-1c87916dc1/0/cache/react-lottie-npm-1.2.3-3d4f0a3852-05326e40b4.zip/node_modules/react-lottie/",\
+          "packageDependencies": [\
+            ["react-lottie", "virtual:7053d59ed2e118e19352cde0aadf8d81d4db3e5773718eb2d5a4970ccb19e3268e049a6cda73bb442eb63ba76c706c33a22452edb2b7d07199c815a77f994eb7#npm:1.2.3"],\
+            ["@types/react", null],\
+            ["babel-runtime", "npm:6.26.0"],\
+            ["lottie-web", "npm:5.12.2"],\
+            ["react", "npm:18.2.0"]\
+          ],\
+          "packagePeers": [\
+            "@types/react",\
+            "react"\
+          ],\
+          "linkType": "HARD"\
+        }]\
+      ]],\
       ["react-redux", [\
         ["npm:8.1.1", {\
           "packageLocation": "./.yarn/cache/react-redux-npm-8.1.1-9d63f7cc0f-3706763307.zip/node_modules/react-redux/",\
@@ -15404,6 +15421,13 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
         }]\
       ]],\
       ["regenerator-runtime", [\
+        ["npm:0.11.1", {\
+          "packageLocation": "./.yarn/cache/regenerator-runtime-npm-0.11.1-a31e4f8dcd-3c97bd2c7b.zip/node_modules/regenerator-runtime/",\
+          "packageDependencies": [\
+            ["regenerator-runtime", "npm:0.11.1"]\
+          ],\
+          "linkType": "HARD"\
+        }],\
         ["npm:0.13.11", {\
           "packageLocation": "./.yarn/cache/regenerator-runtime-npm-0.13.11-90bf536060-27481628d2.zip/node_modules/regenerator-runtime/",\
           "packageDependencies": [\
