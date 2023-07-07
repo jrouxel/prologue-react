83b49ef652c5f483d3ada231845d93ec6d1d4130
FernandoSLuz <51529517+FernandoSLuz@users.noreply.github.com>
2023-07-07 11:06:45 -0400

    updates on recordbutton

---
+++ src/components/footer/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Footer = () => {
+  return (
+    <div>Footer Area</div>
+  )
+};
+
+// Here you could map state values to props if needed
+const mapStateToProps = ({ bot }) => ({
+  // your state values here
+})
+
+// Here you could map dispatch actions to props if needed
+const mapDispatchToProps = dispatch => ({
+  // your dispatch actions here
+})
+
+// Then connect your component to the redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
