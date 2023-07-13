0729e0fdf50f4b9f82eb759bf5d063f7a0e49395
FernandoSLuz <51529517+FernandoSLuz@users.noreply.github.com>
2023-07-12 20:59:36 -0400

    updates

--- src/components/shaderArea/index.js
+++ src/components/shaderArea/index.js
@@ -8,23 +8,24 @@ import * as THREE from 'three';
 import './styles.css';
 
 const ShaderArea = () => {
-    const props = useControls({
-        focus: {value: 5.1, min: 3, max: 7, step: 0.01},
-        speed: {value: 100, min: 0.1, max: 100, step: 0.1},
-        aperture: {value: 1.8, min: 1, max: 5.6, step: 0.1},
-        fov: {value: 50, min: 0, max: 200},
-        curl: {value: 0.25, min: 0.01, max: 0.5, step: 0.01}
-    });
+  const props = useControls({
+    focus: {value: 5.1, min: 3, max: 7, step: 0.01},
+    speed: {value: 100, min: 0.1, max: 100, step: 0.1},
+    aperture: {value: 1.8, min: 1, max: 5.6, step: 0.1},
+    fov: {value: 50, min: 0, max: 200},
+    curl: {value: 0.25, min: 0.01, max: 0.5, step: 0.01}
+  });
 
-    return (
-        <Canvas camera={{fov: 25, position: [0, 0, 6]}} linear={true}
-                gl={{camera: new THREE.WebGL1Renderer({antialias: true, alpha: true})}}>
-            <OrbitControls makeDefault autoRotate autoRotateSpeed={0.5} zoomSpeed={0.1}/>
-            <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5}
-                         maxRoll={0.5} intensity={0.2}/>
-            <Particles {...props} />
-        </Canvas>
-    );
+  return (
+    <Canvas camera={{fov: 25, position: [0, 0, 6]}} linear={true}
+            gl={{camera: new THREE.WebGL1Renderer({antialias: true, alpha: true})}}>
+        
+        <OrbitControls makeDefault autoRotate autoRotateSpeed={0.5} zoomSpeed={0.1}/>
+        <CameraShake yawFrequency={1} maxYaw={0.05} pitchFrequency={1} maxPitch={0.05} rollFrequency={0.5}
+                      maxRoll={0.5} intensity={0.2}/>
+        <Particles {...props}/>
+    </Canvas>
+  );
 };
 // Here you could map state values to props if needed
 const mapStateToProps = ({ bot }) => ({
